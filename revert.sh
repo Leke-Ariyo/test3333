b30c05fc0acc3c5d6c630092c7cf28ea9fba1ddd
orarbel <orarbel@gmail.com>
2022-01-21 17:11:30 +0200

    ....

--- revert.sh
+++ revert.sh
@@ -4,6 +4,7 @@ set -e
 
 # get the SHA to revert
 COMMIT_TO_REVERT=$1
+REPO_FULLNAME=$2
 
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "revert@github.com"
