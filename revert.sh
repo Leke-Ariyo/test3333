b472f6dff4e9b9fcbe65c654481c26f6e94ab031
orarbel <orarbel@gmail.com>
2022-01-21 17:58:18 +0200

    ..

--- revert.sh
+++ revert.sh
@@ -6,10 +6,6 @@ set -e
 COMMIT_TO_REVERT=$1
 REPO_FULLNAME=$2
 
-echo $1
-echo $2
-echo "yo"
-
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 git config --global user.email "revert@github.com"
 git config --global user.name "GitHub Revert Action"
@@ -18,10 +14,5 @@ set -o xtrace
 
 git fetch origin main
 
-# do the revert
-git checkout -b main origin/main
-
-# check commit exists
-git cat-file -t $COMMIT_TO_REVERT
 git revert $COMMIT_TO_REVERT --no-edit
 git push origin main
