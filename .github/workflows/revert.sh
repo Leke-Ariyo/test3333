ee79cb0953f9b7bfa6cc290beda4160d40d6dbf3
orarbel <orarbel@gmail.com>
2022-01-21 16:11:57 +0200

    revert.

---
+++ .github/workflows/revert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# get the SHA to revert
+COMMIT_TO_REVERT=$1
+
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git config --global user.email "revert@github.com"
+git config --global user.name "GitHub Revert Action"
+
+set -o xtrace
+
+git fetch origin master
+
+# do the revert
+git checkout -b master origin/master
+
+# check commit exists
+git cat-file -t $COMMIT_TO_REVERT
+git revert $COMMIT_TO_REVERT --no-edit
+git push origin master
